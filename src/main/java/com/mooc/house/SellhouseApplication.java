1dbd7c413de30107f1eb570b9280796969efe463
cc <1010685161@qq.com>
2019-12-12 20:07:24 +0800

    first commit

---
+++ src/main/java/com/mooc/house/SellhouseApplication.java
@@ -0,0 +1,13 @@
+package com.mooc.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellhouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mooc/house/SellhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooc.house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
